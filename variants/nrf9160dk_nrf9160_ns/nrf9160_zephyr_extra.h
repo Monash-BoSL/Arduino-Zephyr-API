e2cdadef9dc1f44e50ff5b2505d642b9bd2f1d77
dejan <ddeletic@gmail.com>
2023-12-26 18:25:21 +1000

    Added a couple of nrf9160 specific include files.

--- variants/nrf9160dk_nrf9160_ns/nrf9160_zephyr_extra.h
+++ variants/nrf9160dk_nrf9160_ns/nrf9160_zephyr_extra.h
@@ -8,6 +8,8 @@
 #define INCLUDE_NRF1960_ZEPHYR_EXTRA_H
 
 #include <zephyr/kernel.h>
+#include <modem/nrf_modem_lib.h>
+#include <zephyr/drivers/clock_control/nrf_clock_control.h>
 
 #ifdef CONFIG_ADC
     void analogReadResolution (uint8_t bits);
